bd546e576c6d4c5861aac0982ff2a97a266b5722
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-10 16:54:28 +0100

    change main function to use http.HandleFunc

--- main.go
+++ main.go
@@ -5,16 +5,24 @@ import (
 	"net/http"
 )
 
-type server struct{}
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte (`{"message": "get called"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "post called"}`))
+	case "PUT":
+		w.Write
+	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message": "hello world"}`))
 }
 
 func main(){
-	s := &server{}
-	http.Handle("/", s)
+	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":7070", nil))
 }
