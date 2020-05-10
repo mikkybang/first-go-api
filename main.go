7b125fae2b62cb3d826d1c725fe53d4122382537
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-10 16:27:03 +0100

    Add serveHTTP to server struct

--- main.go
+++ main.go
@@ -6,3 +6,15 @@ import (
 )
 
 type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "hello world"}`))
+}
+
+func main(){
+	s := &server{}
+	http.Handle("/", s)
+	log.Fatal(http.ListenAndServe(":7070", nil))
+}
