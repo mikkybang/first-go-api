f87873575c08e86a67e920dd54473f0c6cd2f079
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-11 14:40:48 +0100

    Remove Get request from switch and made into a seperate function

--- main.go
+++ main.go
@@ -29,8 +29,15 @@ func home(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+func get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte (`{"message": "get called"}`))
+}
+
 func main(){
 	r := mux.NewRouter()
+	r.HandleFunc("/", get).Methods(http.MethodGet)
 	r.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":7070", nil))
 }
