8d19685f2cb84a925e342fdbacb927f5aa365b7c
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-11 14:47:26 +0100

    Remove PUT request from switch and made into a seperate function

--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
-
 )
 
-
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
-	case "PUT":
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message": "put called"}`))
 	case "DELETE":
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"message": "delete called"}`))
@@ -26,19 +21,26 @@ func home(w http.ResponseWriter, r *http.Request){
 func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte (`{"message": "get called"}`))
+	w.Write([]byte(`{"message": "get called"}`))
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "post called"}`))
+}
+
+func put(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
 	w.Write([]byte(`{"message": "put called"}`))
 }
 
-func main(){
+func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", get).Methods(http.MethodGet)
-	r.HandleFunc("/" post).Methods(http.MethodPost)
+	r.HandleFunc("/", post).Methods(http.MethodPost)
+	r.HandleFunc("/", put).Methods(http.MethodPut)
 	r.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":7070", nil))
 }
