f767681f90b4e99ae3602d443fcc960918fae011
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-11 14:44:10 +0100

    Remove POST request from switch and made into a seperate function

--- main.go
+++ main.go
@@ -11,12 +11,6 @@ import (
 func home(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte (`{"message": "get called"}`))
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "post called"}`))
 	case "PUT":
 		w.WriteHeader(http.StatusAccepted)
 		w.Write([]byte(`{"message": "put called"}`))
@@ -35,9 +29,16 @@ func get(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte (`{"message": "get called"}`))
 }
 
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "put called"}`))
+}
+
 func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/", get).Methods(http.MethodGet)
+	r.HandleFunc("/" post).Methods(http.MethodPost)
 	r.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":7070", nil))
 }
